84e8152610a29a2ee0240fad454bb1b50221c9b3
Ivan Berdnik <djoistick@gmail.com>
2020-12-28 19:44:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,4 +57,5 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     token                  = data.aws_eks_cluster_auth.cluster.token
     load_config_file       = false
-    }
+  }
+}
