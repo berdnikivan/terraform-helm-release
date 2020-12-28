77a132b935c3825f636eb87c99facabce8d039b0
Ivan Berdnik <djoistick@gmail.com>
2020-12-28 19:11:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ resource helm_release this {
   create_namespace = lookup(var.app, "create_namespace", false)
   skip_crds        = lookup(var.app, "skip_crds", false)
   atomic           = lookup(var.app, "atomic", false)
-  values           = var.values
 
   dynamic "set" {
     iterator = item
@@ -35,3 +34,27 @@ resource helm_release this {
     }
   }
 }
+
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = var.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+  }
+
+provider "helm" {
+  version = "~> 1.0"
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    load_config_file       = false
+    }
